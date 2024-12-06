c2476aee16fd04acba3930a8ee117965f2cd3e32
Yakubjanof712 <husanovaguljahon919@gmail.com>
2024-12-06 11:28:09 +0300

    book
    1

---
+++ book_project/books/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponseNotFound
+from django.shortcuts import render
+from .models import Category, Book
+
+# Create your views here.
+
+def home(request):
+    categories = Category.objects.all()
+    books = Book.objects.all()
+    return render(request, 'home.html', {'categories': categories, 'books': books})
+
+def books_by_category(request, category_id):
+    category = Category.objects.filter(id=category_id).first()
+    if not category:
+        return HttpResponseNotFound("Kategoriya topilmadi!")
+    books = category.book_set.all()
+    return render(request, 'books_by_category.html', {'category': category, 'books': books})
+
+def book_detail(request, book_id):
+    book = Book.objects.filter(id=book_id).first()
+    if not book:
+        return HttpResponseNotFound("Kitob topilmadi!")
+    return render(request, 'book_detail.html', {'book': book})
